4b9f822e034a5f21ea8ef9c7178bf8ffca22db55
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-18 16:50:58 +0300

    app

--- app/src/main/java/com/example/android_weather_app/features/weather_info_show/view/MainActivity.kt
+++ app/src/main/java/com/example/android_weather_app/features/weather_info_show/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.android_weather_app
+package com.example.android_weather_app.features.weather_info_show.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_weather_app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/android_weather_app/features/weather_info_show/viewmodel/WeatherInfoViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.android_weather_app.features.weather_info_show.viewmodel
+
+class WeatherInfoViewModel {
+}
