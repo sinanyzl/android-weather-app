f5c0071a85aee739c4dac3bc004b273d15fed325
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-23 21:45:20 +0300

    app

--- app/src/main/java/com/example/android_weather_app/features/weather_info_show/model/data_class/City.kt
+++ app/src/main/java/com/example/android_weather_app/features/weather_info_show/model/data_class/City.kt
@@ -1,4 +1,14 @@
 package com.example.android_weather_app.features.weather_info_show.model.data_class
 
-class City {
-}
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+
+data class City (
+        @SerializedName("id")
+        val id: Int = 0,
+        @SerializedName("name")
+        val name: String = "",
+        @SerializedName("country")
+        val country: String = ""
+
+): Serializable
