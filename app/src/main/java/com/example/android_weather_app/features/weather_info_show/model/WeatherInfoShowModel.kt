9018900f67b332888d78f858a8f75d006c1530b9
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-24 20:35:05 +0300

    app

--- app/src/main/java/com/example/android_weather_app/features/weather_info_show/model/WeatherInfoShowModel.kt
+++ app/src/main/java/com/example/android_weather_app/features/weather_info_show/model/WeatherInfoShowModel.kt
@@ -1,4 +1,10 @@
 package com.example.android_weather_app.features.weather_info_show.model
 
-class WeatherInfoShowModel {
+import com.example.android_weather_app.common.RequestCompleteListener
+import com.example.android_weather_app.features.weather_info_show.model.data_class.City
+import com.example.android_weather_app.features.weather_info_show.model.data_class.WeatherInfoResponse
+
+interface WeatherInfoShowModel {
+    fun getCityList(callback: RequestCompleteListener<MutableList<City>>)
+    fun getWeatherInfo(cityId: Int, callback: RequestCompleteListener<WeatherInfoResponse>)
 }
