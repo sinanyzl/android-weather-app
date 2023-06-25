8b91d7ec0f199397909f60c3c9830c36470408be
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-06-25 23:22:16 +0300

    app

--- app/src/main/java/com/example/android_weather_app/features/weather_info_show/model/WeatherInfoShowModelImpl.kt
+++ app/src/main/java/com/example/android_weather_app/features/weather_info_show/model/WeatherInfoShowModelImpl.kt
@@ -1,4 +1,18 @@
 package com.example.android_weather_app.features.weather_info_show.model
 
+
+import android.content.Context
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import com.example.android_weather_app.common.RequestCompleteListener
+import com.example.android_weather_app.common.weather_info_show.model.data_class.City
+import com.example.android_weather_app.features.weather_info_show.model.data_class.WeatherInfoResponse
+import com.example.android_weather_app.network.ApiInterface
+import com.example.android_weather_app.RetrofitClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.IOException
+
 class WeatherInfoShowModelImpl {
 }
